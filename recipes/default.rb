42fc4a9cd1bf6aeb05d442be5c946d7e0ac8fbbc
Your Name <you@example.com>
2017-11-14 15:37:04 +0100

    remove apache

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,14 @@ file "/root/myfile" do
 	action :create
 	content "salut testss"
 end
+
+
+execute "update" do
+  command "apt-get update"
+  action :run
+end
+
+execute	"remove-apache" do
+	command "apt-get remove apache2"
+	action	:run
+end
